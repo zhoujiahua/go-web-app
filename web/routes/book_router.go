1b8ee6ba2124d1fa2d7157d432d72db821854c85
jerry.zhou <jiahuasir@163.com>
2023-04-02 12:58:39 +0800

    routes

--- web/routes/book_router.go
+++ web/routes/book_router.go
@@ -2,7 +2,7 @@ package routes
 
 import (
 	"github.com/gin-gonic/gin"
-	"github.com/zhoujiahua/test-app/controllers"
+	"github.com/zhoujiahua/go-web-app/web/routes/controllers"
 )
 
 type BookRouter struct {
